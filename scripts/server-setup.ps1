c2edc0d14e7c95088538c0091492870b0c81a317
David Beale <david.beale@bealearts.co.uk>
2017-04-20 12:59:00 +0100

    Install PS cmdlet

--- scripts/server-setup.ps1
+++ scripts/server-setup.ps1
@@ -1,4 +1,5 @@
 Install-windowsfeature -name AD-Domain-Services;
 
-Install-ADDSForest –DomainName mycompany.local -SafeModeAdministratorPassword (ConvertTo-SecureString -String "YX6oio7nHH+a)X1" -AsPlainText -Force);
+Import-Module ADDSDeployment;
+Install-ADDSForest –DomainName mycompany.local -SafeModeAdministratorPassword (ConvertTo-SecureString -String "YX6oio7nHH+a)X1" -AsPlainText -Force) -Force:$true -NoRebootOnCompletion:$true;
 
